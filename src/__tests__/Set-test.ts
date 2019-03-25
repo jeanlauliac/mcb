c6828bbc809e38ef47a046242000de236f0a278c
Jean Lauliac <jeanlauliac@fb.com>
2019-03-25 17:40:18 +0000

    add Set test

---
+++ src/__tests__/Set-test.ts
@@ -0,0 +1,22 @@
+import Set from '../Set';
+
+let set: Set<number>;
+
+beforeEach(() => {
+  set = new Set(10, (num: number) => num);
+});
+
+it("return false for unknown keys", () => {
+  expect(set.has(3)).toBe(false);
+});
+
+it("tracks a key", () => {
+  set.add(3);
+  expect(set.has(3)).toBe(true);
+});
+
+it("deletes a key", () => {
+  set.add(3);
+  expect(set.delete(3)).toBe(true);
+  expect(set.has(3)).toBe(false);
+});
